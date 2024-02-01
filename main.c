62b40e46950895476dfbbaaec0f6ed2160d184dd
TendoVera <rutsvovera@gmail.com>
2024-02-01 05:38:46 -0800

    mainc

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - entry point for monty.
+ * @argc: number of arguments passed.
+ * @argv: array of argument strings.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    stack_t *top = NULL;
+    unsigned int line_number = 0;
+    ssize_t read_line = 1;
+    char *input;
+    size_t size = 0;
+    FILE *file;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    bus.file = file;
+
+    while (read_line > 0)
+    {
+        input = NULL;
+        read_line = getline(&input, &size, file);
+        bus.input = input;
+        line_number++;
+
+        if (read_line > 0)
+        {
+            execute(input, &top, line_number, file);
+        }
+        free(input);
+    }
+
+    fclose(file);
+    return (0);
+}
